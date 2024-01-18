3741b8755caa452881b9265a9b15c6c4035ca1fd
Kristina Rozanova <zolina.kristina@gmail.com>
2024-01-18 21:24:19 +0200

    controllers for entities were added

---
+++ src/main/java/com/krisroz/demo/controller/InternalReservationController.java
@@ -0,0 +1,10 @@
+package com.krisroz.demo.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/internal/reservations")
+public class InternalReservationController {
+}
