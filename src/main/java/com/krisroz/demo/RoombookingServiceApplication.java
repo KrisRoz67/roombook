27370604bb284663116a6ede3dbd93a3db6619aa
Kristina Rozanova <zolina.kristina@gmail.com>
2024-01-18 17:45:30 +0200

    Initial commit

---
+++ src/main/java/com/krisroz/demo/RoombookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.krisroz.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoombookingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoombookingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krisroz/demo/RoombookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krisroz.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoombookingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
