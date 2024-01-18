fc8adc600a951d3446d502acc8cdaf1cac48c1be
Kristina Rozanova <zolina.kristina@gmail.com>
2024-01-18 21:24:19 +0200

    controllers for entities were added

--- src/main/java/com/krisroz/demo/Application.java
+++ src/main/java/com/krisroz/demo/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RoombookingServiceApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RoombookingServiceApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
---
+++ src/main/java/com/krisroz/demo/controller/ReservationController.java
@@ -0,0 +1,9 @@
+package com.krisroz.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/reservations")
+public class ReservationController {
+}
