b93271304613dd457de41d31a0f33b28e4ae3ee7
Kristina Rozanova <zolina.kristina@gmail.com>
2024-01-18 21:51:57 +0200

    repositories for entities and injection to services were added

--- src/main/java/com/krisroz/demo/repository/ReservationsRepository.java
+++ src/main/java/com/krisroz/demo/repository/ReservationsRepository.java
@@ -3,9 +3,11 @@ package com.krisroz.demo.repository;
 
 import com.krisroz.demo.entity.Customer;
 import com.krisroz.demo.entity.Reservations;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+
 public interface ReservationsRepository extends JpaRepository<Reservations,Long> {
 }
