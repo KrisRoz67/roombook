4cacd9167c78513e134dec50836e9db302067bc0
Kristina Rozanova <zolina.kristina@gmail.com>
2024-01-18 21:42:58 +0200

    inject services to controllers

--- src/main/java/com/krisroz/demo/controller/ReservationController.java
+++ src/main/java/com/krisroz/demo/controller/ReservationController.java
@@ -1,9 +1,14 @@
 package com.krisroz.demo.controller;
 
+import com.krisroz.demo.service.ReservationsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/reservations")
+@RequiredArgsConstructor
 public class ReservationController {
+
+    private final ReservationsService reservationsService;
 }
